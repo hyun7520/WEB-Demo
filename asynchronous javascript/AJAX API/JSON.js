bfb2845e757d7234af4406b25dcc3d05535617bb
hyun7520 <sanghyun123452@gmail.com>
2023-05-17 20:07:24 +0900

    update web study

---
+++ asynchronous javascript/AJAX API/JSON.js
@@ -0,0 +1,14 @@
+// JSON Validator
+// Data recieved from APIs needs to be parsed
+
+JSON.parse(data)
+
+// JSON.stringify : change javascript object to JSON
+
+// XML may still be used but old fashioned
+
+// POSTMAN: help make API calls and test own APIs need sign up
+// HOPPSCOTCH: free
+// https://swapi.dev/ on hoppscotch : returns response
+// https://swapi.dev/api/people/1 retry : returns JSON
+// can also request on browser but displayed in a bad way
