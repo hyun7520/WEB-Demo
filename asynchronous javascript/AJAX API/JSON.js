ab033b964c73ea5c536cca2a335c5b5e0c63d9e7
hyun7520 <sanghyun123452@gmail.com>
2023-05-18 20:03:26 +0900

    update web study

--- asynchronous javascript/AJAX API/JSON.js
+++ asynchronous javascript/AJAX API/JSON.js
@@ -12,3 +12,16 @@ JSON.parse(data)
 // https://swapi.dev/ on hoppscotch : returns response
 // https://swapi.dev/api/people/1 retry : returns JSON
 // can also request on browser but displayed in a bad way
+
+// HTTP Request Verbs
+// 3xx codes: redirection
+// google.co -> redirected to google.com with redirect status code in console
+// 4xx status codes: user or client errors
+// 5xx status codes: server side, API errors
+
+// query string in url
+// starts with question mark : key value pairs seperated by &
+// ex) https://swapi.dev/api/people/1?color=green&aged=23 still works on HOPPSCOTCH
+// API gets needed data from query string but ignores unrequired key value pairs
+
+// headers
