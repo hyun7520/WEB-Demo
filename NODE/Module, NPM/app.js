d72a2d99f53ec4f71e14a135c93cb7f449a8d4db
hyun7520 <sanghyun123452@gmail.com>
2023-05-24 20:06:33 +0900

    update web study

---
+++ NODE/Module, NPM/app.js
@@ -0,0 +1,6 @@
+const math = require('./math.js');
+// need to make sure the reference
+// this case math is in the same directory. so add ./
+console.log(math);
+console.log(math.square(2));
+// returns {} empty
---
+++ NODE/Module, NPM/math.js
@@ -0,0 +1,26 @@
+const add = (x, y) => x + y;
+
+const PI = 3.14159265;
+
+const square = x => x * x;
+
+// only the module.exports will be exported
+// module.exports = "HElllo";
+// set to variabel dont use like this
+
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+// const { PI, square } = require.('./math');
+
+// const math = {
+//     add: add,
+//     PI: PI,
+//     square: square
+// }
+// module.exports = math;
+
+// exports.square = square;
+// exports.PI = PI;
