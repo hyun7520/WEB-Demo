8a27ebc53079f3de740a30cc8e07a6568a8454ad
hyun7520 <sanghyun123452@gmail.com>
2023-06-29 18:32:06 +0900

    update web study

--- NODE/RESTful/index.js
+++ NODE/RESTful/index.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // for every single request 
 // req.body will be now shown
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 // to parse json type body
-app.use(express.json())
+app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'that is funny'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go Swimming'
+    },
+    {
+        username: 'Sk8erboi',
+        comment: 'Plz delete'
+    },
+    {
+        username: 'onlysaywoof',
+        comment: 'WOof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
+
+// why two routes? -> data needs to be sent somewhere as post request
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new')
+})
+
+app.post('/comments', (req, res) => {
+    // could just extract the data from body or push the body on to an array
+    // but this may contain unnecessary stuff or someone can use 'postman' to add fields
+    // so destructure it
+    const { username, comment } = req.body;
+    comments.push({ username, comment })
+    res.send("IT WORKED")
+    // submit comment and check back at /comments
+    // new line will be added
+})
 
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
@@ -22,3 +64,5 @@ app.post('/tacos', (req, res) => {
 app.listen(3000, () => {
     console.log("Listening on port 3000")
 })
+
+// REST -> principals for how client and servers should communicate
