f12a1eb07c35e57c0cad9ec7e0591760376672d6
hyun7520 <sanghyun123452@gmail.com>
2023-08-20 15:35:02 +0900

    upload stduy files
    Node partials and REST

---
+++ NODE/REST/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response")
+})
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    // console.log(req.body);
+    res.send(`orederd ${qty} ${meat} tacos`);
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
--- NODE/Templating_Demo/index.js
+++ NODE/Templating_Demo/index.js
@@ -30,7 +30,7 @@ app.get('/r/:subreddit', (req, res) => {
 
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { rand: num })
+    res.render('random', { num, name: 'random' })
 })
 
 app.listen(3000, () => {
