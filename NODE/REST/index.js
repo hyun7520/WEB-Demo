b3a72ae575e6d68865746955d2c84c5b747850b0
hyun7520 <sanghyun123452@gmail.com>
2023-08-21 19:33:44 +0900

    upload REST study
    RESTful, create and read

--- NODE/REST/index.js
+++ NODE/REST/index.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'that is funny'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erboi',
+        comment: 'plz delete your account Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+app.post('/comments', (req, res) => {
+    // console.log(req.body);
+    // taking whole req.body and pushing it on an array is not a good idea
+    // others can modify or push unnecessary data
+    const { username, comment } = req.body;
+    // so destructure first
+    comments.push({ username, comment });
+    res.send('It worked');
+})
 
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
