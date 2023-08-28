9e47cef7556facff9796243aedbea3e100a9d0be
hyun7520 <sanghyun123452@gmail.com>
2023-08-28 17:47:02 +0900

    upload Mongo stduy
    Mongoose instance, static method, virtual and middleware

--- NODE/Mongo/product.js
+++ NODE/Mongo/product.js
@@ -17,28 +17,121 @@ main()
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        maxlength: 20
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: [0, 'Price must be positive']
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: {
+        type: [String],
+        default: ['cycling']
+    },
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
+    },
+    size: {
+        type: String,
+        enum: ['S', 'M', 'L']
     }
 });
 
+// productSchema.methods.greet = function () {
+//     console.log('Hello');
+//     console.log(`from ${this.name}`)
+// }
+
+productSchema.methods.toggleOnSale = function () {
+    this.onSale = !this.onSale;
+    return this.save();
+}
+
+productSchema.methods.addCategory = function (newCat) {
+    this.categories.push(newCat);
+    return this.save();
+}
+
+productSchema.statics.fireSale = function () {
+    return this.updateMany({}, { onSale: true, price: 0 });
+    // update everything, onsale to true and price to 0
+}
+
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({
-    name: 'Mountain Bike',
-    price: 599,
-    color: 'red'
-})
+const findProduct = async () => {
+    const foundProduct = await Product.findOne({ name: 'Mountain Bike' });
+    // foundProduct.greet();
+    console.log(foundProduct);
+    await foundProduct.toggleOnSale();
+    console.log(foundProduct);
+    await foundProduct.addCategory('Outdoors');
+    console.log(foundProduct);
+}
 
-bike.save()
-    .then(data => {
-        console.log('It worked');
-        console.log(data);
-    })
-    .catch(err => {
-        console.log('Error');
-        console.log(err);
-    })
+Product.fireSale()
+    .then(res =>
+        console.log(res)
+    )
+
+// findProduct();
+
+// const bike = new Product({
+//     name: 'Mountain Bike',
+//     price: 599,
+//     color: 'red'
+//     // color is not in schema - mongo will ignore
+// })
+
+// const bike = new Product({
+//     name: 'Bike Helmet',
+//     price: 29.50,
+//     // default for onSale set to false
+//     categories: ['Cycling', 'safety']
+// })
+
+// enum
+// const bike = new Product({
+//     name: 'Cycling Jersey',
+//     price: 28.50,
+//     categories: ['Cycling'],
+//     size: 'XS'
+    // throws validations error as 'XS' in not int size enum
+// })
+
+// bike.save()
+//     .then(data => {
+//         console.log('It worked');
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log('Error');
+//         console.log(err);
+//     })
+
+// Product.findOneAndUpdate(
+//     { name: 'Tire Pump' },
+//     { price: -99 },
+//     { new: true, runValidators: true }
+//     // tell mongoose to apply validations when updating
+// )
+//     .then(data => {
+//         console.log('It worked');
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log('Error');
+//         console.log(err);
+//     })
