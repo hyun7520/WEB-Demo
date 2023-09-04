7436300ebd732baf8a130c50118c01232991cf94
hyun7520 <sanghyun123452@gmail.com>
2023-09-04 13:29:11 +0900

    upload Yelpcamp progress
    Finish basic setup. Update and Delete method

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/YelpCamp', {
@@ -20,7 +21,8 @@ main()
         console.log(err);
     });
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -40,15 +42,35 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
+// submit form to create new campground and redirect
 app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+// show details of campground when clicked
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
+})
+
+// move to edit form page
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 
 app.listen(3000, () => {
