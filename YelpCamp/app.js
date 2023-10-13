f91aff8afd363f84d6839b0eaad1087addeb47ff
hyun7520 <sanghyun123452@gmail.com>
2023-10-13 15:14:43 +0900

    Update app.js

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -31,9 +31,9 @@ const mongoSanitize = require('express-mongo-sanitize');
 
 const helmet = require('helmet');
 
-// const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL;
 
-const dbUrl = 'mongodb://127.0.0.1:27017/YelpCamp'
+// const dbUrl = 'mongodb://127.0.0.1:27017/YelpCamp'
 
 // mongodb://127.0.0.1:27017/YelpCamp - local url
 async function main() {
